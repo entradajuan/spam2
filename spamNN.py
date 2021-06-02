13cf12c03cd05321b93d7d0c9963ed852b139337
entradajuan <entradajuan@yahoo.es>
2021-06-02 09:00:22 +0000

    developing a NN spam detector

--- spamNN.py
+++ spamNN.py
@@ -1,3 +1,10 @@
-!pip install transformers
+%tensorflow_version 2.x
+import tensorflow as tf
+#from tf.keras.models import Sequential
+#from tf.keras.layers import Dense
+import os
+import io
+
+tf.__version__
 
 
