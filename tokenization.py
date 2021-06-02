f8dbcea606e4da41685bd78912bc3f4b7dcf2d28
entradajuan <entradajuan@yahoo.es>
2021-06-02 17:30:36 +0000

    playing with tokenization

--- tokenization.py
+++ tokenization.py
@@ -3,3 +3,17 @@ splited_sentence = sentence.split()
 print(splited_sentence)
 print(len(splited_sentence))
 print(type(splited_sentence))
+
+
+!pip install stanza
+import stanza
+en = stanza.download('en')
+en = stanza.Pipeline(lang='en')
+print(en)
+print(type(en))
+
+tokenized = en(sentence)
+print(tokenized.sentences[0])
+print(len(tokenized.sentences))
+print(sentence)
+
