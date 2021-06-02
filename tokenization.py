5f5d0eff53ab2be4321ee337a5117a3e27e78e2b
entradajuan <entradajuan@yahoo.es>
2021-06-02 17:48:32 +0000

    playing with tokenization

--- tokenization.py
+++ tokenization.py
@@ -4,7 +4,6 @@ print(splited_sentence)
 print(len(splited_sentence))
 print(type(splited_sentence))
 
-
 !pip install stanza
 import stanza
 en = stanza.download('en')
@@ -16,4 +15,22 @@ tokenized = en(sentence)
 print(tokenized.sentences[0])
 print(len(tokenized.sentences))
 print(sentence)
+
+for sen in tokenized.sentences:
+  for w in sen.tokens:
+    print(w.text)
+
+en2 = stanza.Pipeline(lang='en')
+tokenized_sent_2 = en2('Hari went to school')
+print(tokenized_sent_2.sentences[0].tokens[0])
+print(type(tokenized_sent_2.sentences[0].tokens[0]))
+
+dicts = tokenized_sent_2.to_dict()
+obj1 = dicts[0][0]['xpos']
+print(obj1)
+print(type(obj1))
+
+for sen in dicts:
+  for words in sen:
+    print(words['text'], words['lemma'], words['xpos'])
 
