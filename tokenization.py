19f966315f7dc3fca156c257e39ddb28469085fe
entradajuan <entradajuan@yahoo.es>
2021-06-02 17:17:53 +0000

    playing with tokenization

---
+++ tokenization.py
@@ -0,0 +1,5 @@
+sentence = 'Looking for a PART-TIME Consultant in Public Health! Interim role from the end of June until March next year.'
+splited_sentence = sentence.split()
+print(splited_sentence)
+print(len(splited_sentence))
+print(type(splited_sentence))
